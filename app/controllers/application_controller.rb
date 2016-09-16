d3c76ae3066b0abc3ade02fe88a4d87282dfa311
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-09-16 11:39:14 -0300

    definindo policies forma do padrão de modelos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,28 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def policy(record, record_identifier = nil)
+    identifier = record_identifier || record
+    policies[identifier] ||= begin
+                               policy = Pundit::PolicyFinder.new(identifier).policy!
+                               policy.new(current_user, record)
+                             end
+  end
+
+  def authorize(record, options = {})
+    query = options.fetch :query, "#{params[:action]}?"
+    record_idenfifier = options[:record_idenfifier]
+
+    @_pundit_policy_authorized = true
+
+    policy = policy(record, record_idenfifier)
+    unless policy.public_send(query)
+      raise NotAuthorizedError.new(query: query, record: record, policy: policy)
+    end
+
+    true
+  end
+
   private
 
   def user_not_authorized
